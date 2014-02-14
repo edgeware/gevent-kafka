74f5b471005779a2331d55ed2a98d52d9e5934cf
Peter Renström <renstrom.peter@gmail.com>
2014-02-14 15:32:26 +0100

    Move dependencies from requirements.txt to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
+kwargs = {
+    'name': 'gevent-kafka',
+    'version': '0.2.2',
+    'description': 'ApacheKafka bindings for gevent',
+    'author': 'Johan Rydberg',
+    'author_email': 'johan.rydberg@gmail.com',
+    'url': 'https://github.com/edgeware/gevent-kafka',
+    'packages': ['gevent_kafka'],
+    'install_requires': [
+        'gevent==0.13.8',
+        'kazoo==1.2.1'
+    ]
+}
 
-setup(name='gevent-kafka',
-      version='0.2.2',
-      description='ApacheKafka bindings for gevent',
-      author='Johan Rydberg',
-      author_email='johan.rydberg@gmail.com',
-      url='https://github.com/jrydberg/gevent-kafka',
-      packages=['gevent_kafka']
-     )
+setup(**kwargs)
