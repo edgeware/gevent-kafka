e2ee2caa6a23a226dfd6d80810e6a2ea3c51ba5c
Peter Renström <peter.renstrom@edgeware.tv>
2014-04-10 17:07:03 +0200

    Add support for running tests from setup.py.

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
-kwargs = {
-    'name': 'gevent-kafka',
-    'version': '0.2.2',
-    'description': 'ApacheKafka bindings for gevent',
-    'author': 'Johan Rydberg',
-    'author_email': 'johan.rydberg@gmail.com',
-    'url': 'https://github.com/edgeware/gevent-kafka',
-    'packages': ['gevent_kafka'],
-    'install_requires': [
-        'gevent==0.13.8',
-        'kazoo==1.2.1'
-    ]
-}
-
-setup(**kwargs)
+setup(name='gevent-kafka',
+      version='0.2.2',
+      description='Apache Kafka bindings for Gevent',
+      author='Edgeware',
+      author_email='info@edgeware.tv',
+      url='https://github.com/edgeware/gevent-kafka',
+      packages=find_packages(),
+      test_suite='gevent_kafka.test',
+      install_requires=[
+          'gevent==0.13.8',
+          'kazoo==1.2.1'
+      ],
+      tests_require=[
+          'mock==1.0.1'
+      ])
